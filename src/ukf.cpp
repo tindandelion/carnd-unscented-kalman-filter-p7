46ff04c738e928ac9384ad55cb2502e0fbd85e0d
Sergey Moshnikov <s.moshnikov@gmail.com>
2017-10-31 05:44:45 +0200

    Cleaning up filter initialization

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -39,50 +39,25 @@ UKF::UKF() {
   n_aug_ = n_x_ + 2;
   lambda_ = 3 - n_aug_;
   
-  // if this is false, laser measurements will be ignored (except during init)
   use_laser_ = true;
-
-  // if this is false, radar measurements will be ignored (except during init)
   use_radar_ = true;
 
-  // initial state vector
-  x_ = VectorXd(n_x_);
-
-  // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
-
-  // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
-
-  // Laser measurement noise standard deviation position1 in m
-  std_laspx_ = 0.15;
-
-  // Laser measurement noise standard deviation position2 in m
-  std_laspy_ = 0.15;
-
-  // Radar measurement noise standard deviation radius in m
-  std_radr_ = 0.3;
-
-  // Radar measurement noise standard deviation angle in rad
-  std_radphi_ = 0.03;
-
-  // Radar measurement noise standard deviation radius change in m/s
-  std_radrd_ = 0.3;
-
   is_initialized_ = false;
-
+  x_ = VectorXd(n_x_);
   P_ = 10 * MatrixXd::Identity(n_x_, n_x_);
 
-  Xsig_pred_ = MatrixXd::Zero(n_x_, 2 * n_aug_ + 1);
-  weights_ = VectorXd::Zero(Xsig_pred_.cols());
-  
-  for(int i = 0; i < weights_.size(); i++) {
-    if (i == 0) {
-      weights_[i] = lambda_ / (lambda_ + n_aug_);
-    } else {
-      weights_[i] = 1 / (2 * (lambda_ + n_aug_));
-    }
+  Xsig_pred_ = MatrixXd::Zero(n_x_, 2*n_aug_+1);
+  weights_ = VectorXd(2*n_aug_+1);
+
+  weights_[0] = lambda_ / (lambda_ + n_aug_);
+  for(int i = 1; i < weights_.size(); i++) {
+    weights_[i] = 1 / (2 * (lambda_ + n_aug_));
   }
+  
+  Q_ = MatrixXd(2, 2);
+  Q_ <<
+    std_a_*std_a_, 0,
+    0, std_yawdd_*std_yawdd_;
 
   R_laser_ = MatrixXd(2, 2);
   R_laser_ <<
@@ -94,12 +69,7 @@ UKF::UKF() {
     std_radr_*std_radr_, 0, 0,
     0, std_radphi_*std_radphi_, 0,
     0, 0, std_radrd_*std_radrd_;
-
-  std_a_ = 0.5;
-  std_yawdd_ = M_PI / 6;
 }
-
-UKF::~UKF() {}
 
 void UKF::Initialize(const MeasurementPackage& meas_package) {
   if (meas_package.sensor_type_ == MeasurementPackage::SensorType::LASER) {
@@ -119,14 +89,10 @@ void UKF::Initialize(const MeasurementPackage& meas_package) {
 MatrixXd UKF::GenerateSigmaPoints() {
   VectorXd x_aug = VectorXd::Zero(n_aug_);
   MatrixXd P_aug = MatrixXd::Zero(n_aug_, n_aug_);
-  MatrixXd Q = MatrixXd(2, 2);
-  Q <<
-    std_a_*std_a_, 0,
-    0, std_yawdd_*std_yawdd_;
   
   x_aug.head(n_x_) = x_;
   P_aug.topLeftCorner(n_x_, n_x_) = P_;
-  P_aug.bottomRightCorner(2, 2) = Q;
+  P_aug.bottomRightCorner(2, 2) = Q_;
 
   MatrixXd P_sqrt = P_aug.llt().matrixL();
   MatrixXd sigma_common = sqrt(lambda_ + n_aug_) * P_sqrt;
@@ -205,11 +171,6 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
   }
 }
 
-/**
- * Predicts sigma points, the state, and the state covariance matrix.
- * @param {double} delta_t the change in time (in seconds) between the last
- * measurement and this one.
- */
 void UKF::Prediction(double delta_t) {
   MatrixXd X_sigma = GenerateSigmaPoints();
   PredictSigmaPoints(X_sigma, delta_t);
--- src/ukf.h
+++ src/ukf.h
@@ -34,26 +34,17 @@ public:
   ///* time when the state is true, in us
   long long time_us_;
 
-  ///* Process noise standard deviation longitudinal acceleration in m/s^2
-  double std_a_;
 
-  ///* Process noise standard deviation yaw acceleration in rad/s^2
-  double std_yawdd_;
+  const double std_a_ = 0.5;  ///* Process noise standard deviation longitudinal acceleration in m/s^2
+  const double std_yawdd_ = M_PI / 6;   ///* Process noise standard deviation yaw acceleration in rad/s^2
 
-  ///* Laser measurement noise standard deviation position1 in m
-  double std_laspx_;
 
-  ///* Laser measurement noise standard deviation position2 in m
-  double std_laspy_;
+  const double std_laspx_ = 0.15;   ///* Laser measurement noise standard deviation position1 in m
+  const double std_laspy_ = 0.15;   ///* Laser measurement noise standard deviation position2 in m
 
-  ///* Radar measurement noise standard deviation radius in m
-  double std_radr_;
-
-  ///* Radar measurement noise standard deviation angle in rad
-  double std_radphi_;
-
-  ///* Radar measurement noise standard deviation radius change in m/s
-  double std_radrd_ ;
+  const double std_radr_ = 0.3;   ///* Radar measurement noise standard deviation radius in m
+  const double std_radphi_ = 0.03;   ///* Radar measurement noise standard deviation angle in rad
+  const double std_radrd_ = 0.3;   ///* Radar measurement noise standard deviation radius change in m/s
 
   ///* Weights of sigma points
   VectorXd weights_;
@@ -67,51 +58,29 @@ public:
   ///* Sigma point spreading parameter
   double lambda_;
 
+  ///* Noise matrices
+  MatrixXd Q_;
   MatrixXd R_laser_;
   MatrixXd R_radar_;
 
-
-  /**
-   * Constructor
-   */
   UKF();
-
-  /**
-   * Destructor
-   */
-  virtual ~UKF();
 
   /**
    * ProcessMeasurement
    * @param meas_package The latest measurement data of either radar or laser
    */
   void ProcessMeasurement(MeasurementPackage meas_package);
-
-  /**
-   * Prediction Predicts sigma points, the state, and the state covariance
-   * matrix
-   * @param delta_t Time between k and k+1 in s
-   */
-  void Prediction(double delta_t);
-
-  /**
-   * Updates the state and the state covariance matrix using a laser measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateLidar(const VectorXd& z_meas);
-
-  /**
-   * Updates the state and the state covariance matrix using a radar measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateRadar(const VectorXd&  z_meas);
-
+  
 private:
   void Initialize(const MeasurementPackage& meas_package);
+  void Prediction(double delta_t);
   MatrixXd GenerateSigmaPoints();
   void PredictSigmaPoints(const MatrixXd& X_sigma, double delta_t);
   void PredictState();
+  
   void Update(const MeasurementPackage& meas_package);
+  void UpdateLidar(const VectorXd& z_meas);
+  void UpdateRadar(const VectorXd& z_meas);
 };
 
 #endif /* UKF_H */
